9fafa5f072d5c18f976dd9b78387945b897d847f
hoshinogami <3108764593@qq.com>
2023-01-17 16:16:54 +0800

    success

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -2,41 +2,48 @@ import React from "react";
 import  "./current-weather.css";
 import {TRANSLATE_API_URL,TRANSLATE_API_KEY,TRANSLATE_API_ID} from "../../api"
 import md5 from 'js-md5';
-import fetchJSONP from 'fetch-jsonp'
-import { useState } from "react";
+import fetchJSONP from 'fetch-jsonp';
+import { useState ,useEffect} from "react";
+//import City from './city';
 const CurrentWeather =({data})=>{
-  const[city,setCity]=useState(null);
-  const dst=(item)=>{
-    
-   const salt = Math.round(Math.random(10000));
+ const[city,setCity]=useState(null);
+ const [description,setDescription]=useState(null);
+ //let city;
+ useEffect(()=>{
+ const dst =async(item)=>{
+    const salt = Math.round(Math.random(10000));
    const content=item;
    let sign=TRANSLATE_API_ID+content+salt+TRANSLATE_API_KEY;
    sign=md5(sign);
    const translateurl=TRANSLATE_API_URL+'?appid=' + TRANSLATE_API_ID + '&q=' + content + '&from=en&to=zh&salt=' + salt + '&sign=' + sign;
  
-  let des1;
- fetchJSONP(translateurl,{mode:'no-cors',
+  //let des1;
+ let res= await fetchJSONP(translateurl,{mode:'no-cors',
    credentials: 'include',
    method: 'GET',
    headers:{ Accept: 'application/json',},
    jsonCallbackFunction:'showLocation'},)
  
-   .then(async(response) =>{
-    const des=await response.json();
-    des1=des;
-      console.log(des);
-  })
-  .catch(console.log);
-  
-   return des1;
+   let  des=await res.json();
+   console.log(des)
+   let dst=await  JSON.stringify(des.trans_result[0].dst);
+   setCity(dst);
 }
-
-console.log( dst(data.city));
+ dst(data.city)
+ dst(data.weather[0].description)
+},[data])
+   
+   //return des1;
+//}
+console.log(city);
+//setCity(des1().trans_result[0].dst);
+//const det=await dst(data.city);
  return (
       <div className="weather">
         <div className="top">
           <div>
-          <p className="city">{data.city}</p>
+          <p className="city">{city}</p>
+         
             <p className="weather-description">{data.weather[0].description}</p>
           </div>
           <img alt="weather" src={`icons/${data.weather[0].icon}.png`}/>
